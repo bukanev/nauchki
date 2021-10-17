2a09020ba1d352c00090c0a813002d41db053624
RailYunusov <opel808437@gmail.com>
2021-10-17 19:44:10 +0300

    Добавил CorsConfig

---
+++ src/main/java/com/example/nauchki/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.nauchki.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("POST, GET, OPTIONS, DELETE, HEAD")
+                        .allowedHeaders("*").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+}
