0c9c0de61fd24fa5a35effc16a2f40a1480bf53f
Elena <rozetta.95@mail.ru>
2022-05-12 20:46:58 +0300

    Replacing the div block with a link in the header logo

--- nauchki-react-main/src/components/Header/Header.js
+++ nauchki-react-main/src/components/Header/Header.js
@@ -1,18 +1,17 @@
-import React from "react";
-import { FaRegUserCircle } from "react-icons/fa";
-import { AiOutlineHome } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
-import logo from "../../img/logo_nauchki.svg";
+import React from 'react';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { AiOutlineHome } from 'react-icons/ai';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../img/logo_nauchki.svg';
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="header__content _container">
         <div className="header__title">
-          <div className="header__logo">
-
-         <img src={logo} className="header__App-logo" alt="logo" />
-          </div>
+          <Link to="/" className="header__logo">
+            <img src={logo} className="header__App-logo" alt="logo" />
+          </Link>
         </div>
         <div>
           <ul className="header__menu">
@@ -25,7 +24,6 @@ export const Header = () => {
             <li className="header__menu-item">
               <NavLink to="/">Источники данных</NavLink>
             </li>
-
           </ul>
         </div>
         <div>
