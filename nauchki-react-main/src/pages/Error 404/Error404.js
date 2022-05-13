81f432429d342650df6da0870760b16adde9910f
Elena <rozetta.95@mail.ru>
2022-05-13 22:47:11 +0300

    A 404 error component was created

--- nauchki-react-main/src/pages/Error 404/Error404.js
+++ nauchki-react-main/src/pages/Error 404/Error404.js
@@ -1,12 +1,17 @@
-import React, { useCallback, useState } from "react";
-import { AdminAPI } from "../../api/api";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './error404.scss';
 
-export const Feedback = () => {
-
-    return (
-      <div className="_wrapper area">
-          <h1> О проекте</h1>
-   </div>
+export const Error404 = () => {
+  return (
+    <section className="error">
+      <div className="error__info">
+        <h1 className="error__title">404</h1>
+        <p className="error__text">Видимо вы заблудились, такой страницы не существует</p>
+        <NavLink to="/" className="error__link">
+          <span>Главная</span>
+        </NavLink>
+      </div>
+    </section>
   );
 };
-
