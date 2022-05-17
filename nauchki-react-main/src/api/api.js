ad61a266a92410b3b2056afdf0448bcfb0130a12
Elena <rozetta.95@mail.ru>
2022-05-17 23:35:21 +0300

    edit api

--- nauchki-react-main/src/api/api.js
+++ nauchki-react-main/src/api/api.js
@@ -1,52 +1,59 @@
-import * as axios from "axios";
-import {
-  Redirect
-} from "react-router";
-
+import * as axios from 'axios';
+import { Redirect } from 'react-router';
 
 const instance = axios.create({
   withCredentials: true,
   baseURL: 'http://89.108.88.2:8080',
 });
 
-
 class Api {
   static request(url, data, method, useToken = true) {
-    return instance.request({
-      method,
-      url,
-      data,
-      headers: useToken ? {
-        'Authorization': localStorage.getItem('TOKEN')
-      } : undefined
-    }).catch(e => {
-    });
+    return instance
+      .request({
+        method,
+        url,
+        data,
+        headers: useToken
+          ? {
+              Authorization: localStorage.getItem('TOKEN'),
+            }
+          : undefined,
+      })
+      .catch((e) => {});
   }
 
   static post(url, data, useToken) {
-    return Api.request(url, data, "POST", useToken);
+    return Api.request(url, data, 'POST', useToken);
   }
 
   static get(url, useToken) {
-    return Api.request(url, "GET", useToken);
+    return Api.request(url, 'GET', useToken);
   }
 }
 
 export const UserAPI = {
   getAuthUser(login, password) {
-    return Api.post(`/user`, {
-      email: login,
-      password: password,
-    }, true);
+    return Api.post(
+      `/user`,
+      {
+        login: login,
+        password: password,
+      },
+      true,
+    );
   },
 };
 
 export const LoginAPI = {
   auth(login, password) {
-    return Api.post(`/auth`, {
-      email: login,
-      password: password,
-    }, false);
+    return Api.post(
+      `/auth`,
+      {
+        login: login,
+        password: password,
+      },
+      false,
+    );
   },
 };
 
@@ -70,7 +77,7 @@ export const AdminAPI = {
 
 export const PostsAPI = {
   getPosts(tag) {
-    return instance.get(`/posts${tag !== undefined ? `/${tag}` : ""}`);
+    return instance.get(`/posts${tag !== undefined ? `/${tag}` : ''}`);
   },
   getTags() {
     return instance.get(`/tags`);
