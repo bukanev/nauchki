8a566634f4a4d057d6d423b7623deaa454b984f2
Elena <rozetta.95@mail.ru>
2022-05-17 21:53:12 +0300

    Transferring styles to components

--- nauchki-react-main/src/components/Landing/Landing.js
+++ nauchki-react-main/src/components/Landing/Landing.js
@@ -1,8 +1,6 @@
-import React from "react";
+import React from 'react';
+import './landing.scss';
 
 export const Landing = () => {
-    return (
-        <section className="landingpage _wrapper"></section>
-
-    )
-}
+  return <section className="landingpage _wrapper"></section>;
+};
--- nauchki-react-main/src/components/Tariff/Tariff.js
+++ nauchki-react-main/src/components/Tariff/Tariff.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React from 'react';
+import './tariff.scss';
 
 export const Tariff = () => {
-   return (
-     <section className="tariff _wrapper">
-       <button className="tariff_profil">250 руб.</button>
-       <button className="tariff_profil">400 руб.</button>
-     </section>
-   );
+  return (
+    <section className="tariff _wrapper">
+      <button className="tariff_profil">250 руб.</button>
+      <button className="tariff_profil">400 руб.</button>
+    </section>
+  );
 };
