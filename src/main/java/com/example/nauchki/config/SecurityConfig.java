6b82739d05f1ff8e7c59de6b6ec633875fb8267b
RailYunusov <opel808437@gmail.com>
2022-05-17 22:13:59 +0300

    edit jwt and security

--- src/main/java/com/example/nauchki/config/SecurityConfig.java
+++ src/main/java/com/example/nauchki/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .antMatchers("/registration", "/reg","/static/**", "/activate/*", "/test", "/auth",
                         "index.html","/swagger-ui", "/editpassword/**","editpass").permitAll()
                 .antMatchers("/del/{spring:[0-9]+}", "/user/{spring:[0-9]+}","/stage/**","/admin","/admin/**").hasAnyAuthority("ADMIN","SUPERADMIN")
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
--- src/main/java/com/example/nauchki/jwt/JwtProvider.java
+++ src/main/java/com/example/nauchki/jwt/JwtProvider.java
@@ -3,7 +3,6 @@ package com.example.nauchki.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.nauchki.model.User;
 import com.google.common.base.Strings;
@@ -19,7 +18,6 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 
