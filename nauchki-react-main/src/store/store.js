dc0f4b958738384e7ec99239dcc3ab7a41ea0f13
Elena <rozetta.95@mail.ru>
2022-05-24 18:30:20 +0300

    add reducer

--- nauchki-react-main/src/store/store.js
+++ nauchki-react-main/src/store/store.js
@@ -1,18 +1,19 @@
-import { combineReducers, createStore, applyMiddleware } from "redux";
-import { compose } from "redux";
-import { postsReducer } from "./postsReducer";
-import { userReducer } from "./userReducer";
-import { childrenInputReducer } from "./OnechildInput";
-import storage from "redux-persist/lib/storage"; // localStorage
-import { persistStore, persistReducer } from "redux-persist";
-import { childrenReducer } from "./childrenReducer";
-import thunk from "redux-thunk";
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { compose } from 'redux';
+import { postsReducer } from './postsReducer';
+import { userReducer } from './userReducer';
+import { childrenInputReducer } from './OnechildInput';
+import storage from 'redux-persist/lib/storage'; // localStorage
+import { persistStore, persistReducer } from 'redux-persist';
+import { childrenReducer } from './childrenReducer';
+import thunk from 'redux-thunk';
+import { recoveryPassReducer } from './recoveryPassReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // создаем объект конфигурации для persist
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage,
 };
 const rootReducer = combineReducers({
@@ -20,6 +21,7 @@ const rootReducer = combineReducers({
   posts: postsReducer,
   children: childrenReducer,
   phase: childrenInputReducer,
+  recoveryPass: recoveryPassReducer,
 });
 
 // оборачиваем редьюсеры в persist
@@ -27,7 +29,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = createStore(
   persistedReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk)),
 );
 
 // создаем persistor
