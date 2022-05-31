14be8a7368fe8fe949c279d1d0636228da8ca6e7
Elena <rozetta.95@mail.ru>
2022-05-31 16:22:37 +0300

    edit

--- nauchki-react-main/src/index.js
+++ nauchki-react-main/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
-console.log(new Date());
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
