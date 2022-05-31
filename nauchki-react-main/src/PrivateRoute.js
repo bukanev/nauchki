6eaf34b370a34bc5b888d199f5aecbaabb93f77b
Elena <rozetta.95@mail.ru>
2022-05-31 14:53:48 +0300

    added selector

--- nauchki-react-main/src/PrivateRoute.js
+++ nauchki-react-main/src/PrivateRoute.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Login } from "./pages/Login/Login";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Login } from './pages/Login/Login';
+import { selectIsAuth } from './store/user/selectors';
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
-  const isAuth = useSelector((state) => state.user.isAuth);
+  const isAuth = useSelector(selectIsAuth);
 
   return (
     <Route
@@ -19,4 +20,3 @@ export const PrivateRoute = ({ component: Component, ...rest }) => {
     />
   );
 };
-
