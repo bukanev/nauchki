80b8b6a6f6c625f5bffc30d66ba14128e6bb3d3e
Elena <rozetta.95@mail.ru>
2022-06-08 16:02:51 +0300

    remove children reducer

--- nauchki-react-main/src/store/children/selectors.js
+++
@@ -1,1 +0,0 @@
-export const selectChildren = (state) => state.withPersist.children.children;
--- nauchki-react-main/src/store/withPersistReducers.js
+++ nauchki-react-main/src/store/withPersistReducers.js
@@ -4,12 +4,10 @@ import { postsReducer } from './posts/reducer';
 import { userReducer } from './user/reducer';
 import { childrenInputReducer } from './childrenInput/reducer';
 import { UserChildrenReducer } from './userChildren/reducer';
-import { childrenReducer } from './children/reducer';
 
 export const withPersistReducers = combineReducers({
   user: userReducer,
   posts: postsReducer,
-  children: childrenReducer,
   phase: childrenInputReducer,
   userChildren: UserChildrenReducer,
 });
