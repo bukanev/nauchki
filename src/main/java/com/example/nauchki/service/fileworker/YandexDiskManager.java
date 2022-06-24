60f7b7d72d51a7b479991db623eebc1c48af3efe
Ladatko <timal1@yandex.ru>
2022-06-24 10:12:16 +0300

    code Review

--- src/main/java/com/example/nauchki/service/fileworker/YandexDiskManager.java
+++ src/main/java/com/example/nauchki/service/fileworker/YandexDiskManager.java
@@ -2,7 +2,6 @@ package com.example.nauchki.service.fileworker;
 
 import lombok.extern.log4j.Log4j2;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
@@ -26,29 +25,29 @@ public class YandexDiskManager implements UploadAndDeleteFileManager {
 
     private String token;
     private String cloudUrl;
-    private HttpURLConnection connection;
+    private final String requestUrlPublish = "/resources/publish?path=";
+    private final String requestUrlGetMetaInfo = "/resources?path=";
+    private final String requestUrlGetUpload = "/resources/upload?path=/";
 
     @Autowired
-    public YandexDiskManager(@Value("${yandex.box.token}") String token,
-                             @Value("${yandex.box.cloudUrl}") String cloudUrl) {
+    public YandexDiskManager(@Value("${yandex.token}") String token,
+                             @Value("${yandex.path}") String cloudUrl) {
         this.token = token;
         this.cloudUrl = cloudUrl;
     }
 
     @Override
-    public String saveFile(MultipartFile file, String ExternalId) {
+    public String saveFile(MultipartFile file, String externalId) {
+
         String path = "";
 
         try {
-            String uploadUrl = getUploadLink(ExternalId);
+            String uploadUrl = requestHTTP(externalId, requestUrlGetUpload, "GET", 200);
 
-            File convFile = new File(ExternalId);
-            FileOutputStream fos = null;
-
-            fos = new FileOutputStream(convFile);
+            File convFile = new File(externalId);
+            FileOutputStream fos = new FileOutputStream(convFile);
             fos.write(file.getBytes());
             fos.close();
-
 
             HttpEntity entity = MultipartEntityBuilder.create()
                     .addPart("file", new FileBody(convFile))
@@ -58,127 +57,65 @@ public class YandexDiskManager implements UploadAndDeleteFileManager {
             request.setEntity(entity);
 
             HttpClient client = HttpClientBuilder.create().build();
-            HttpResponse response = null;
+            client.execute(request);
 
-            response = client.execute(request);
-
-            if (connection.getResponseCode() != 200) {
-                log.info(String.format("Response code:%s , message:%s",
-                        connection.getResponseCode(), connection.getResponseMessage()));
-            }
-
-            publishFile(ExternalId);
-            path = getPublishLink(ExternalId);
+            requestHTTP(externalId, requestUrlPublish, "PUT", 200);
+            path = requestHTTP(externalId, requestUrlGetMetaInfo, "GET", 200);
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            connection.disconnect();
         }
         return path;
-    }
-
-    public String getUploadLink(String ExternalId) throws IOException {
-
-        String requestUrl = "/resources/upload?path=/" + ExternalId;
-        String requestMethod = "GET";
-
-        getConnection(requestMethod, requestUrl);
-
-        if (connection.getResponseCode() == 200) {
-            JSONObject jsonObject = getJsonFromResponse();
-            return jsonObject.getString("href");
-        }
-        log.info(
-                String.format("Response code:%s , message:%s",
-                        connection.getResponseCode(), connection.getResponseMessage()));
-        return null;
-    }
-
-
-    public void publishFile(String fileName) throws IOException {
-
-        String requestUrl = "/resources/publish?path=" + fileName;
-        String requestMethod = "PUT";
-
-        getConnection(requestMethod, requestUrl);
-
-        if (connection.getResponseCode() != 200) {
-            log.info(String.format("Response code:%s , message:%s",
-                    connection.getResponseCode(), connection.getResponseMessage()));
-        }
-    }
-
-    public String getPublishLink(String ExternalId) throws IOException {
-
-        String requestUrl = "/resources?path=" + ExternalId;
-        String requestMethod = "GET";
-
-        getConnection(requestMethod, requestUrl);
-
-        if (connection.getResponseCode() == 200) {
-            JSONObject jsonObject = getJsonFromResponse();
-            return jsonObject.getString("public_url");
-        }
-        log.info(
-                String.format("Response code when get publish Link:%s , message:%s",
-                        connection.getResponseCode(), connection.getResponseMessage()));
-        return connection.getResponseMessage();
     }
 
     @Override
     public boolean deleteFile(String ExternalId) {
 
-        String requestUrl = "/resources?path=" + ExternalId;
-        String requestMethod = "DELETE";
-
         try {
-            getConnection(requestMethod, requestUrl);
-
+            HttpURLConnection connection = getConnection("DELETE", requestUrlGetMetaInfo + ExternalId, 204);
             if (connection.getResponseCode() == 204) {
                 return true;
             }
-            log.info(
-                    String.format("Response code when delete file:%s , message:%s",
-                            connection.getResponseCode(), connection.getResponseMessage()));
-
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            connection.disconnect();
         }
         return false;
     }
 
-    //TODO при регистрации пользователя создавать его папку на Яндекс диске
-    // для того чтобы сохранять файлы каждому пользователю в его папку
-    // на Яндекс диске, в метод надо передавать email пользователя
-    public void createDirectory(String email) {
+    public HttpURLConnection getConnection(String requestMethod, String requestUrl, Integer expectedResponseCode) throws IOException {
 
-        String requestUrl = "/resources?path=" + email;
-        String requestMethod = "PUT";
-
-        try {
-            getConnection(requestMethod, requestUrl);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            connection.disconnect();
-        }
-    }
-
-    public void getConnection(String requestMethod, String requestUrl) throws IOException {
-
+        HttpURLConnection connection;
         URL url = new URL(cloudUrl + requestUrl);
         connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod(requestMethod);
         connection.setRequestProperty("Authorization", "OAuth " + token);
         connection.setUseCaches(false);
         connection.setDoOutput(true);
+        if (connection.getResponseCode() != expectedResponseCode) {
+            log.info(
+                    String.format("Response code when get publish Link:%s , message:%s",
+                            connection.getResponseCode(), connection.getResponseMessage()));
+            return null;
+        }
+        return connection;
     }
 
-    public JSONObject getJsonFromResponse() throws IOException {
+    public String requestHTTP(String externalId, String requestUrl, String requestMethod, Integer expectedResponseCode) throws IOException {
+
+        HttpURLConnection connection = getConnection(requestMethod, requestUrl + externalId, expectedResponseCode);
+        JSONObject jsonObject = getJsonFromResponse(connection);
+
+        switch (requestUrl) {
+            case requestUrlGetMetaInfo:
+                return jsonObject.getString("public_url");
+            case requestUrlGetUpload:
+                return jsonObject.getString("href");
+            default:
+                return null;
+        }
+    }
+
+    public JSONObject getJsonFromResponse(HttpURLConnection connection) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         StringBuilder sb = new StringBuilder();
         String line;
@@ -190,3 +127,4 @@ public class YandexDiskManager implements UploadAndDeleteFileManager {
         return jsonObject;
     }
 }
+
