fe14278aba0f2a9fe2237f03e5eaf4b7a31e3f13
Elena <rozetta.95@mail.ru>
2022-06-25 12:43:41 +0300

    added adminPostReducer

--- nauchki-react-main/src/store/withPersistReducers.js
+++ nauchki-react-main/src/store/withPersistReducers.js
@@ -4,10 +4,12 @@ import { postsReducer } from './posts/reducer';
 import { userReducer } from './user/reducer';
 import { childrenInputReducer } from './childrenInput/reducer';
 import { UserChildrenReducer } from './userChildren/reducer';
+import { adminPostReducer } from './adminka/reducer';
 
 export const withPersistReducers = combineReducers({
   user: userReducer,
   posts: postsReducer,
   phase: childrenInputReducer,
   userChildren: UserChildrenReducer,
+  admin: adminPostReducer,
 });
