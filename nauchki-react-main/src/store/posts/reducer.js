cb66c17edc9d20ff4849b080ef7d01b7403ea15e
Elena <rozetta.95@mail.ru>
2022-06-26 10:39:31 +0300

    added new actions: ADD_POST, EDIT_POST,DELETE_POST, POST_LOADING, ERROR_POST added thunk: addPostThunk, removePostThunk added new methods in reducers|: ADD_POST, DELETE_POST

--- nauchki-react-main/src/store/posts/reducer.js
+++ nauchki-react-main/src/store/posts/reducer.js
@@ -1,23 +1,29 @@
-import { GET_POSTS, GET_TAGS } from "./actions";
+import { ADD_POST, DELETE_POST, GET_POSTS, GET_TAGS } from './actions';
 
 const initialState = {
   posts: [],
-  tags: []
+  tags: [],
 };
 
-export const postsReducer = (state = initialState, action) => {
-  switch (action.type) {
+export const postsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case GET_POSTS:
       return {
         ...state,
-        posts: action.payload
+        posts: payload,
       };
     case GET_TAGS:
       return {
         ...state,
-        tags: action.payload,
+        tags: payload,
       };
-
+    case ADD_POST:
+      return {
+        ...state,
+        ...payload,
+      };
+    case DELETE_POST:
+      return state.filter(({ id }) => id !== payload);
     default:
       return state;
   }
