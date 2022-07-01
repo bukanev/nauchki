3e69de0facb0610a3533e868d7f52bf1110f3a58
Elena <rozetta.95@mail.ru>
2022-07-01 18:50:36 +0300

    edited initialState. add case ERROR_AUTH

--- nauchki-react-main/src/store/user/reducer.js
+++ nauchki-react-main/src/store/user/reducer.js
@@ -1,23 +1,25 @@
-import { GET_USER_DATA, TOGGLE_AUTH } from './actions';
+import {
+  ERROR_AUTH,
+  GET_USER_DATA,
+  TOGGLE_AUTH,
+} from './actions';
 
 const initialState = {
-  userData: {},
   isAuth: false,
+  data: null,
+  error: null,
 };
 
-export const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case GET_USER_DATA:
-      return {
-        ...state,
-        userData: action.payload,
-      };
+export const userReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case TOGGLE_AUTH:
-      return {
-        ...state,
-        isAuth: action.payload,
-      };
+      return { ...state, isAuth: payload };
+    case GET_USER_DATA:
+      return { ...state, data: payload };
+    case ERROR_AUTH:
+      return { ...state, error: payload };
     default:
+      console.log(state);
       return state;
   }
 };
