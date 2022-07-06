13952e7d8e8b21562b4211efdcf6d8b181a4ea6e
alexey-polyashov <polyashofff@yandex.ru>
2022-07-06 23:14:36 +0300

    add PostRepo test

---
+++ src/test/java/com/example/nauchki/PostRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.example.nauchki;
+
+import com.example.nauchki.model.Post;
+import com.example.nauchki.repository.PostRepo;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class PostRepositoryTest {
+
+    @Autowired
+    private TestEntityManager tem;
+
+    @Autowired
+    private PostRepo postRepo;
+
+    @Test
+    public void findAllTagTest(){
+        tem.persist(Post.builder()
+                .tag("test1")
+                .title("Test post")
+                .build()
+        );
+        tem.persist(Post.builder()
+                .tag("test2")
+                .title("Test post")
+                .build()
+        );
+        List<String> allTags = Arrays.asList("test2", "test1");
+        List<String> findTags = postRepo.findAllTag();
+        Assertions.assertTrue(allTags.size() == findTags.size() && allTags.containsAll(findTags) && findTags.containsAll(allTags));
+    }
+
+    @Test
+    public void findByTagTest(){
+        Post p1 = Post.builder()
+                .tag("test1")
+                .title("Test post")
+                .build();
+        Post p2 = Post.builder()
+                .tag("test2")
+                .title("Test post")
+                .build();
+        tem.persist(p1);
+        tem.persist(p2);
+
+        List<Post> allTags = Arrays.asList(p1);
+
+        List<Post> findTags = postRepo.findByTag("test1");
+        Assertions.assertTrue(allTags.size() == findTags.size() && allTags.containsAll(findTags) && findTags.containsAll(allTags));
+
+        findTags = postRepo.findByTag("test3");
+        Assertions.assertEquals(0, findTags.size());
+
+    }
+
+}
