1f3045697829d8321aca6f0b36c5190e26868da1
Alaya95 <rozetta.95@mail.ru>
2022-07-19 09:49:42 +0300

    edited

--- nauchki-react-main/src/Router.js
+++ nauchki-react-main/src/Router.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import { Main } from './pages/Main/Main';
-import { Login } from './pages/Login/Login';
-import { Registration } from './pages/Registration/Registration';
-import { ResetPassword } from './pages/ResetPassword/ResetPassword';
-import { RecoveryPassword } from './pages/RecoveryPassword/RecoveryPassword';
+import { Main } from "./pages/Main/Main";
+import { Login } from "./pages/Login/Login";
+import { Registration } from "./pages/Registration/Registration";
+import { ResetPassword } from "./pages/ResetPassword/ResetPassword";
+import { RecoveryPassword } from "./pages/RecoveryPassword/RecoveryPassword";
 
-import { PersonalArea } from './pages/PersonalArea/PersonalArea';
-import { PrivateRoute } from './PrivateRoute';
-import { Adminka } from './pages/Adminka/Adminka';
-import { Articles } from './pages/Articles/Articles';
-import { OneChildren } from './pages/PersonalArea/OneChildren';
-import { Error404 } from './pages/Error 404/Error404';
-import { AdminArticlesTable } from './components/AdminArticlesTable/AdminArticlesTable';
-import AdminArticlesForm from './components/AdminArticlesForm/AdminArticlesForm';
+import { PersonalArea } from "./pages/PersonalArea/PersonalArea";
+import { PrivateRoute } from "./PrivateRoute";
+import { Adminka } from "./pages/Adminka/Adminka";
+import { Articles } from "./pages/Articles/Articles";
+import { OneChildren } from "./pages/PersonalArea/OneChildren";
+import { Error404 } from "./pages/Error 404/Error404";
+import { AdminArticlesTable } from "./components/AdminArticlesTable/AdminArticlesTable";
+import AdminArticlesForm from "./components/AdminArticlesForm/AdminArticlesForm";
 
 export const Router = () => {
-
   return (
     <Routes>
       <Route path="/" element={<Main />}></Route>
@@ -26,10 +25,10 @@ export const Router = () => {
       <Route path={`aboutproject`} element={<h1>О проекте</h1>}></Route>
       <Route path={`tariff`} element={<h1>Тарифы</h1>}></Route>
 
-      <Route path="/registration" component={Registration} />
+      <Route path="/registration" element={<Registration />} />
       <Route path="/login" element={<Login />} />
-      <Route exact path="/recoverypass" component={RecoveryPassword} />
-      <Route exact path="/resetpass" component={ResetPassword} />
+      <Route exact path="/recoverypass" element={<RecoveryPassword />} />
+      <Route exact path="/resetpass" element={<ResetPassword />} />
 
       <Route exact path="/personalArea" element={<PrivateRoute />}>
         <Route path="" element={<PersonalArea />}></Route>
@@ -39,12 +38,28 @@ export const Router = () => {
       <Route path={`/adminka`} element={<PrivateRoute />}>
         <Route path="" element={<Adminka />}>
           <Route index path="articles" element={<AdminArticlesTable />}></Route>
-          <Route index path="articles/edit/:id" element={<AdminArticlesForm />}></Route>
-          <Route index path="articles/add" element={<AdminArticlesForm />}></Route>
+          <Route
+            index
+            path="articles/edit/:id"
+            element={<AdminArticlesForm />}
+          ></Route>
+          <Route
+            index
+            path="articles/add"
+            element={<AdminArticlesForm />}
+          ></Route>
 
           <Route index path="stages" element={<AdminArticlesTable />}></Route>
-          <Route index path="stages/edit/:id" element={<AdminArticlesForm />}></Route>
-          <Route index path="stages/add" element={<AdminArticlesForm />}></Route>
+          <Route
+            index
+            path="stages/edit/:id"
+            element={<AdminArticlesForm />}
+          ></Route>
+          <Route
+            index
+            path="stages/add"
+            element={<AdminArticlesForm />}
+          ></Route>
 
           {/* <Route index path="feedback" element={<h1>Обратная связь</h1>}></Route> */}
         </Route>
